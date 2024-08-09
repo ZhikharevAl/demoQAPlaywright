c66127d466e419f994683d69e18c3a0679f9e61e
AlexeyZh <waltafunk@gmail.com>
2024-08-09 05:17:12 +0300

    feat: add BaseTest

---
+++ src/test/kotlin/BaseTest.kt
@@ -0,0 +1,32 @@
+import com.microsoft.playwright.Browser
+import com.microsoft.playwright.BrowserType
+import com.microsoft.playwright.Page
+import com.microsoft.playwright.Playwright
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+
+open class BaseTest {
+    protected lateinit var page: Page
+    private lateinit var browser: Browser
+    private lateinit var playwright: Playwright
+
+    @BeforeEach
+    fun setup() {
+        playwright = Playwright.create()
+        browser =
+            playwright.chromium().launch(
+                BrowserType.LaunchOptions().apply {
+                    headless = false
+                },
+            )
+        page = browser.newPage()
+        page.navigate("https://demoqa.com/automation-practice-form")
+    }
+
+    @AfterEach
+    fun teardown() {
+        page.close()
+        browser.close()
+        playwright.close()
+    }
+}
